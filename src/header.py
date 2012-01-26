f7283c28733701e58c27f7bee02e114eb0799065
Brian Warner <warner@lothar.com>
2012-01-26 08:35:08 -0800

    release 0.6

--- src/header.py
+++ src/header.py
@@ -7,7 +7,7 @@
 * https://github.com/warner/python-versioneer
 * Brian Warner
 * License: Public Domain
-* Version: 0.5
+* Version: 0.6
 
 This file helps distutils-based projects manage their version number by just
 creating version-control tags.
