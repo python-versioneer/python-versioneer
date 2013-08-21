e1d59ce4000a71b0e99c1f79f8c1f1978e90b505
Brian Warner <warner@lothar.com>
2013-08-20 22:22:59 -0700

    handle refnames made by git-1.8.3 and later
    Git-1.8.3 introduced a change to the way "$Format" strings (specifically
    the "%d" variable) are expanded in git-archive -generated tarballs. The
    list of refs has always included both branch names and tags. But in
    older versions, tags were not emitted any differently than branch names:
    the list might say:
    
     (master, HEAD, v1.0)
    
    Starting in 1.8.3, tags are marked with a distinctive "tag: " prefix, so
    you get:
    
     (master, HEAD, tag: v1.0)
    
    The versioneer code that deduces version tags from generated tarballs
    was not spotting the newly-marked tags, and falling back to reporting a
    full SHA1 hash as the revision, instead of the intended tag name.
    
    This change fixes the parser and restores compatibility with newer
    versions of git. Closes: #11.

--- src/git/middle.py
+++ src/git/middle.py
@@ -30,20 +30,24 @@ def versions_from_expanded_variables(variables, tag_prefix, verbose=False):
             print("variables are unexpanded, not using")
         return {} # unexpanded, so not in an unpacked git-archive tarball
     refs = set([r.strip() for r in refnames.strip("()").split(",")])
-    for ref in list(refs):
-        if not re.search(r'\d', ref):
-            if verbose:
-                print("discarding '%s', no digits" % ref)
-            refs.discard(ref)
-            # Assume all version tags have a digit. git's %d expansion
-            # behaves like git log --decorate=short and strips out the
-            # refs/heads/ and refs/tags/ prefixes that would let us
-            # distinguish between branches and tags. By ignoring refnames
-            # without digits, we filter out many common branch names like
-            # "release" and "stabilization", as well as "HEAD" and "master".
+    # starting in git-1.8.3, tags are listed as "tag: foo-1.0" instead of
+    # just "foo-1.0". If we see a "tag: " prefix, prefer those.
+    TAG = "tag: "
+    tags = set([r[len(TAG):] for r in refs if r.startswith(TAG)])
+    if not tags:
+        # Either we're using git < 1.8.3, or there really are no tags. We use
+        # a heuristic: assume all version tags have a digit. The old git %d
+        # expansion behaves like git log --decorate=short and strips out the
+        # refs/heads/ and refs/tags/ prefixes that would let us distinguish
+        # between branches and tags. By ignoring refnames without digits, we
+        # filter out many common branch names like "release" and
+        # "stabilization", as well as "HEAD" and "master".
+        tags = set([r for r in refs if re.search(r'\d', r)])
+        if verbose:
+            print("discarding '%s', no digits" % ",".join(refs-tags))
     if verbose:
-        print("remaining refs: %s" % ",".join(sorted(refs)))
-    for ref in sorted(refs):
+        print("likely tags: %s" % ",".join(sorted(tags)))
+    for ref in sorted(tags):
         # sorting will prefer e.g. "2.0" over "2.0rc1"
         if ref.startswith(tag_prefix):
             r = ref[len(tag_prefix):]
