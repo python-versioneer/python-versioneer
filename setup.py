e0cf1918bdc30266872f820b4a8aaae638adf16d
Brian Warner <warner@lothar.com>
2014-03-11 12:46:49 -0700

    remove pre-generated versioneer.py, build in setup.py instead
    Normally ("setup.py build") we only create versioneer.py in memory, then
    embed it into bin/versioneer-installer. If you want a standalone
    copy (for testing or debugging), you can use the new "setup.py
    make_versioneer" command, which will dump a copy in the current
    directory.
    
    This also updates the .travis.yml file to make_versioneer before running
    test/test_git.py, since that test assumes it can find a copy of
    versioneer.py in the top directory.

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 
-import os, base64, tempfile
-from distutils.core import setup
+import os, base64, tempfile, StringIO
+from distutils.core import setup, Command
 from distutils.command.build_scripts import build_scripts
 
 LONG="""
@@ -10,10 +10,60 @@ the conventional 'from PROJECT import _version' pattern) by asking your
 version-control system about the current tree.
 """
 
+def get(fn):
+    return open(fn, "r").read()
+def unquote(s):
+    return s.replace("%", "%%")
+def ver(s):
+    return s.replace("@VERSIONEER@", "0.8+")
+def readme(s):
+    return s.replace("@README@", get("README.md"))
+
+def generate_versioneer():
+    vcs = "git"
+    if vcs not in ("git",):
+        raise ValueError("Unhandled revision-control system '%s'" % vcs)
+    f = StringIO.StringIO()
+    f.write(readme(ver(get("src/header.py"))))
+    f.write('VCS = "%s"\n' % vcs)
+    f.write("\n\n")
+    for line in open("src/%s/long-version.py" % vcs, "r").readlines():
+        if line.startswith("#### START"):
+            f.write("LONG_VERSION_PY = '''\n")
+        elif line.startswith("#### SUBPROCESS_HELPER"):
+            f.write(unquote(get("src/subprocess_helper.py")))
+        elif line.startswith("#### MIDDLE"):
+            f.write(unquote(get("src/%s/middle.py" % vcs)))
+        elif line.startswith("#### PARENTDIR"):
+            f.write(unquote(get("src/parentdir.py")))
+        elif line.startswith("#### END"):
+            f.write("'''\n")
+        else:
+            f.write(ver(line))
+    f.write(get("src/subprocess_helper.py"))
+    f.write(get("src/%s/middle.py" % vcs))
+    f.write(get("src/parentdir.py"))
+    f.write(get("src/%s/install.py" % vcs))
+    f.write(ver(get("src/trailer.py")))
+    return f.getvalue()
+
+
+class make_versioneer(Command):
+    description = "create standalone versioneer.py"
+    user_options = []
+    boolean_options = []
+    def initialize_options(self):
+        pass
+    def finalize_options(self):
+        pass
+    def run(self):
+        with open("versioneer.py", "w") as f:
+            f.write(generate_versioneer())
+        return 0
+
 class my_build_scripts(build_scripts):
     def run(self):
-        with open("versioneer.py") as f:
-            v = f.read()
+        v = generate_versioneer()
         v_b64 = base64.b64encode(v)
         lines = [v_b64[i:i+60] for i in range(0, len(v_b64), 60)]
         v_b64 = "\n".join(lines)+"\n"
@@ -46,5 +96,7 @@ setup(
     # otherwise it skips that step.
     scripts = ["fake"],
     long_description = LONG,
-    cmdclass = { "build_scripts": my_build_scripts },
+    cmdclass = { "build_scripts": my_build_scripts,
+                 "make_versioneer": make_versioneer,
+                 },
     )
