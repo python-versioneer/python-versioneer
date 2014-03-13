33794afef0dca502858218097f6659942c9a1111
Brian Warner <warner@lothar.com>
2014-03-12 17:25:12 -0700

    installer: mention if we're overwriting an existing copy

--- src/installer.py
+++ src/installer.py
@@ -1,12 +1,14 @@
 #!/usr/bin/env python
 
-import base64
+import os, base64
 
 VERSIONEER_b64 = """
 @VERSIONEER-INSTALLER@
 """
 
 v = base64.b64decode(VERSIONEER_b64)
+if os.path.exists("versioneer.py"):
+    print "overwriting existing versioneer.py"
 with open("versioneer.py", "w") as f:
     f.write(v)
 print "versioneer.py installed into local tree"
