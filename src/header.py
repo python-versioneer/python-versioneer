13cb24b9b2567667fb2e4c1c580912ef6eb5033f
Brian Warner <warner@lothar.com>
2014-03-21 22:43:26 -0700

    header.py: add notes

--- src/header.py
+++ src/header.py
@@ -10,6 +10,8 @@ from distutils.core import Command
 from distutils.command.sdist import sdist as _sdist
 from distutils.command.build import build as _build
 
+# these configuration settings will be overridden by setup.py after it
+# imports us
 versionfile_source = None
 versionfile_build = None
 tag_prefix = None
