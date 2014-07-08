0d97d3585d23c220290f365cdfd66d43777bc44a
Brian Warner <warner@lothar.com>
2014-07-08 11:51:37 -0700

    test pyflakes-ness of generated _version.py too

---
+++ test/flakesapp/setup.py
@@ -0,0 +1,21 @@
+
+from distutils.core import setup
+import versioneer
+versioneer.versionfile_source = "src/demo/_version.py"
+versioneer.versionfile_build = "demo/_version.py"
+versioneer.tag_prefix = "demo-"
+versioneer.parentdir_prefix = "demo-"
+versioneer.VCS = "git"
+commands = versioneer.get_cmdclass().copy()
+
+setup(name="demo",
+      version=versioneer.get_version(),
+      description="Demo",
+      url="url",
+      author="author",
+      author_email="email",
+      packages=["demo"],
+      package_dir={"demo": "src/demo"},
+      scripts=["bin/rundemo"],
+      cmdclass=commands,
+      )
