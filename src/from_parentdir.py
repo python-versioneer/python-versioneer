ca61e41eaa995848c6741ad52d4131a3aec89b0d
Jeff McBride <mcbridejc@gmail.com>
2015-01-10 13:43:09 -0800

    When searching for version from parent directory name, search up the hierarchy two levels

--- src/from_parentdir.py
+++ src/from_parentdir.py
@@ -5,15 +5,23 @@ class NotThisMethod(Exception): pass  # --STRIP DURING BUILD
 def versions_from_parentdir(parentdir_prefix, root, verbose):
     """Try to determine the version from the parent directory name.
 
-    Source tarballs conventionally unpack into a directory that includes
-    both the project name and a version string.
+    Source tarballs conventionally unpack into a directory that includes both
+    the project name and a version string. We will also support searching up
+    two directory levels for an appropriately named parent directory
     """
-    dirname = os.path.basename(root)
-    if not dirname.startswith(parentdir_prefix):
-        if verbose:
-            print("guessing rootdir is '%s', but '%s' doesn't start with "
-                  "prefix '%s'" % (root, dirname, parentdir_prefix))
-        raise NotThisMethod("rootdir doesn't start with parentdir_prefix")
-    return {"version": dirname[len(parentdir_prefix):],
-            "full-revisionid": None,
-            "dirty": False, "error": None}
+    rootdirs = []
+
+    for i in range(3):
+        dirname = os.path.basename(root)
+        if dirname.startswith(parentdir_prefix):
+            return {"version": dirname[len(parentdir_prefix):],
+                    "full-revisionid": None,
+                    "dirty": False, "error": None}
+        else:
+            rootdirs.append(root)
+            root = os.path.dirname(root) # up a level
+
+    if verbose:
+        print("Tried directories %s but none started with prefix %s" %
+            (str(rootdirs), parentdir_prefix) )
+    raise NotThisMethod("rootdir doesn't start with parentdir_prefix")
