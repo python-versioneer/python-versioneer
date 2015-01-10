6c9f4ad191242cfcd37613dc80d46888656dd134
Jeff McBride <mcbridejc@gmail.com>
2015-01-10 13:43:09 -0800

    When searching for version from parent directory name, search up the hierarchy two levels

--- src/from_parentdir.py
+++ src/from_parentdir.py
@@ -1,11 +1,18 @@
 
 def versions_from_parentdir(parentdir_prefix, root, verbose=False):
     # Source tarballs conventionally unpack into a directory that includes
-    # both the project name and a version string.
-    dirname = os.path.basename(root)
-    if not dirname.startswith(parentdir_prefix):
-        if verbose:
-            print("guessing rootdir is '%s', but '%s' doesn't start with prefix '%s'" %
-                  (root, dirname, parentdir_prefix))
-        return None
-    return {"version": dirname[len(parentdir_prefix):], "full": ""}
+    # both the project name and a version string. We will also support searching
+    # up two directory levels for an appropriately named parent directory
+    rootdirs = []
+
+    for i in range(3):
+        dirname = os.path.basename(root)
+        if dirname.startswith(parentdir_prefix):
+            return {"version": dirname[len(parentdir_prefix):], "full": ""}
+        else:
+            rootdirs.append(root)
+            root = os.path.dirname(root) # up a level
+    if verbose:
+        print("Tried directories %s but none started with prefix %s" %
+            (str(rootdirs), parentdir_prefix) )
+    return None
