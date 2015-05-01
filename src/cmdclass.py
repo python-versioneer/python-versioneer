1ee0f840700bba1e29126cdc9e891edda29328c6
Brian Warner <warner@lothar.com>
2015-05-01 00:53:10 -0700

    cmdclass: use config object, remove verbose=

--- src/cmdclass.py
+++ src/cmdclass.py
@@ -12,19 +12,20 @@ class cmd_version(Command):
         pass
 
     def run(self):
-        ver = get_version(verbose=True)
+        ver = get_version()
         print("Version is currently: %s" % ver)
 
 
 class cmd_build(_build):
     def run(self):
-        versions = get_versions(verbose=True)
+        cfg = get_config()
+        versions = get_versions()
         _build.run(self)
         # now locate _version.py in the new build/ directory and replace it
         # with an updated value
-        if versionfile_build:
+        if cfg.versionfile_build:
             target_versionfile = os.path.join(self.build_lib,
-                                              versionfile_build)
+                                              cfg.versionfile_build)
             print("UPDATING %s" % target_versionfile)
             write_to_version_file(target_versionfile, versions)
 
@@ -33,36 +34,38 @@ if 'cx_Freeze' in sys.modules:  # cx_freeze enabled?
 
     class cmd_build_exe(_build_exe):
         def run(self):
-            versions = get_versions(verbose=True)
-            target_versionfile = versionfile_source
+            cfg = get_config()
+            versions = get_versions()
+            target_versionfile = cfg.versionfile_source
             print("UPDATING %s" % target_versionfile)
             write_to_version_file(target_versionfile, versions)
 
             _build_exe.run(self)
             os.unlink(target_versionfile)
-            with open(versionfile_source, "w") as f:
-                assert VCS is not None, "please set versioneer.VCS"
-                LONG = LONG_VERSION_PY[VCS]
+            with open(cfg.versionfile_source, "w") as f:
+                assert cfg.VCS is not None, "please set versioneer.VCS"
+                LONG = LONG_VERSION_PY[cfg.VCS]
                 f.write(LONG % {"DOLLAR": "$",
-                                "TAG_PREFIX": tag_prefix,
-                                "PARENTDIR_PREFIX": parentdir_prefix,
-                                "VERSIONFILE_SOURCE": versionfile_source,
+                                "TAG_PREFIX": cfg.tag_prefix,
+                                "PARENTDIR_PREFIX": cfg.parentdir_prefix,
+                                "VERSIONFILE_SOURCE": cfg.versionfile_source,
                                 })
 
 
 class cmd_sdist(_sdist):
     def run(self):
-        versions = get_versions(verbose=True)
+        versions = get_versions()
         self._versioneer_generated_versions = versions
         # unless we update this, the command will keep using the old version
         self.distribution.metadata.version = versions["version"]
         return _sdist.run(self)
 
     def make_release_tree(self, base_dir, files):
+        cfg = get_config()
         _sdist.make_release_tree(self, base_dir, files)
         # now locate _version.py in the new base_dir directory (remembering
         # that it may be a hardlink) and replace it with an updated value
-        target_versionfile = os.path.join(base_dir, versionfile_source)
+        target_versionfile = os.path.join(base_dir, cfg.versionfile_source)
         print("UPDATING %s" % target_versionfile)
         write_to_version_file(target_versionfile,
                               self._versioneer_generated_versions)
