de3a7c1105a5a5088bb3588f0a2bdff8e3a16f9f
Brian Warner <warner@lothar.com>
2015-05-25 12:04:30 -0700

    subprocess_helper: fix error reporting for arg-less commands

--- src/subprocess_helper.py
+++ src/subprocess_helper.py
@@ -6,6 +6,7 @@ def run_command(commands, args, cwd=None, verbose=False, hide_stderr=False):
     p = None
     for c in commands:
         try:
+            dispcmd = str([c] + args)
             # remember shell=False, so use git.cmd on windows, not just git
             p = subprocess.Popen([c] + args, cwd=cwd, stdout=subprocess.PIPE,
                                  stderr=(subprocess.PIPE if hide_stderr
@@ -16,7 +17,7 @@ def run_command(commands, args, cwd=None, verbose=False, hide_stderr=False):
             if e.errno == errno.ENOENT:
                 continue
             if verbose:
-                print("unable to run %s" % args[0])
+                print("unable to run %s" % dispcmd)
                 print(e)
             return None
     else:
@@ -28,6 +29,6 @@ def run_command(commands, args, cwd=None, verbose=False, hide_stderr=False):
         stdout = stdout.decode()
     if p.returncode != 0:
         if verbose:
-            print("unable to run %s (error)" % args[0])
+            print("unable to run %s (error)" % dispcmd)
         return None
     return stdout
