6bf878820a34f12aa4481944476183cde18bd03b
Marcel Huber <marcelhuberfoo@gmail.com>
2016-06-01 10:29:51 +0200

    another flake8 fix

--- src/render.py
+++ src/render.py
@@ -164,5 +164,6 @@ def render(pieces, style):
         raise ValueError("unknown style '%s'" % style)
 
     return {"version": rendered, "full-revisionid": pieces["long"],
-            "dirty": pieces["dirty"], "error": None, "date": pieces.get("date","")}
+            "dirty": pieces["dirty"], "error": None,
+            "date": pieces.get("date", "")}
 
