bcae362201df31697ca626754c64ec43140330b0
Christopher J. Markiewicz <markiewicz@stanford.edu>
2020-12-06 09:05:28 -0500

    RF: Use standard method to avoid branch

--- src/header.py
+++ src/header.py
@@ -99,9 +99,7 @@ def register_vcs_handler(vcs, method):  # decorator
     """Create decorator to mark a method as the handler of a VCS."""
     def decorate(f):
         """Store f in HANDLERS[vcs][method]."""
-        if vcs not in HANDLERS:
-            HANDLERS[vcs] = {}
-        HANDLERS[vcs][method] = f
+        HANDLERS.setdefault(vcs, {})[method] = f
         return f
     return decorate
 
