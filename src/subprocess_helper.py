db7963abd00fdafcc1eefc5c7b9e8bf5c44d9299
tanvimoharir <74228962+tanvimoharir@users.noreply.github.com>
2021-05-31 22:09:35 +0530

    Lint fix for 'consider-using-with' (#257)

--- src/subprocess_helper.py
+++ src/subprocess_helper.py
@@ -1,5 +1,5 @@
 import sys, subprocess, errno # --STRIP DURING BUILD
-# pylint:disable=too-many-arguments # noqa
+# pylint:disable=too-many-arguments,consider-using-with # noqa
 def run_command(commands, args, cwd=None, verbose=False, hide_stderr=False,
                 env=None):
     """Call the given command(s)."""
