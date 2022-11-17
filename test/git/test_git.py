7a85be96879de4d55362cbc17322424cfcf32d56
TrellixVulnTeam <charles.mcfarland@trellix.com>
2022-11-17 21:56:31 +0000

    Adding tarfile member sanitization to extractall()

--- test/git/test_git.py
+++ test/git/test_git.py
@@ -708,7 +708,26 @@ class Repo(common.Common, unittest.TestCase):
         fn = os.path.join(dist_path, distfile)
         os.mkdir(self.subpath("out/TF"))
         with tarfile.open(fn) as t:
-            t.extractall(path=self.subpath("out/TF"))
+            def is_within_directory(directory, target):
+                
+                abs_directory = os.path.abspath(directory)
+                abs_target = os.path.abspath(target)
+            
+                prefix = os.path.commonprefix([abs_directory, abs_target])
+                
+                return prefix == abs_directory
+            
+            def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+            
+                for member in tar.getmembers():
+                    member_path = os.path.join(path, member.name)
+                    if not is_within_directory(path, member_path):
+                        raise Exception("Attempted Path Traversal in Tar File")
+            
+                tar.extractall(path, members, numeric_owner=numeric_owner) 
+                
+            
+            safe_extract(t, path=self.subpath("out/TF"))
         target = self.subpath("out/TF/demo-%s" % exps["TF"][0])
         self.assertTrue(os.path.isdir(target))
         self.check_version(target, state, "TF", exps["TF"])
