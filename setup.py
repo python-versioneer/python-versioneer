754e634f7f7298db70245a5c4586203f72ee4bce
Justin Wood <jwood@anaconda.com>
2023-04-21 23:52:51 -0400

    Type check the root setup.py for versioneer

--- setup.py
+++ setup.py
@@ -3,9 +3,10 @@
 import os, base64, tempfile, io
 from importlib import util as ilu
 from pathlib import Path
+from typing import List, Tuple
 from setuptools import setup, Command
 from setuptools.command.build_py import build_py
-from setuptools.command.develop import develop
+from setuptools.command.develop import develop as _develop
 
 # If versioneer is not installed in the environment, then we will need to
 # need to build and exec it. The build requires a VERSION, so we might need
@@ -13,10 +14,16 @@ from setuptools.command.develop import develop
 VERSION = "0+bootstrap"
 
 
-def ver(s):
+def ver(s: str) -> str:
     return s.replace("@VERSIONEER-VERSION@", VERSION)
 
-def get(fn, add_ver=False, unquote=False, do_strip=False, do_readme=False):
+def get(
+    fn: str,
+    add_ver: bool = False,
+    unquote: bool = False,
+    do_strip: bool = False,
+    do_readme: bool = False
+) -> str:
     with open(fn) as f:
         text = f.read()
 
@@ -31,14 +38,14 @@ def get(fn, add_ver=False, unquote=False, do_strip=False, do_readme=False):
         text = text.replace("@README@", get("README.md"))
     return text
 
-def get_vcs_list():
+def get_vcs_list() -> List[str]:
     project_path = Path(__file__).absolute().parent / "src"
     return [filename
             for filename
             in os.listdir(str(project_path))
             if Path.is_dir(project_path / filename) and filename != "__pycache__"]
 
-def generate_long_version_py(VCS):
+def generate_long_version_py(VCS: str) -> str:
     s = io.StringIO()
     s.write(get(f"src/{VCS}/long_header.py", add_ver=True, do_strip=True))
     for piece in ["src/subprocess_helper.py",
@@ -50,7 +57,7 @@ def generate_long_version_py(VCS):
         s.write(get(piece, unquote=True, do_strip=True))
     return s.getvalue()
 
-def generate_versioneer_py():
+def generate_versioneer_py() -> bytes:
     s = io.StringIO()
     s.write(get("src/header.py", add_ver=True, do_readme=True, do_strip=True))
     s.write(get("src/subprocess_helper.py", do_strip=True))
@@ -78,26 +85,25 @@ def generate_versioneer_py():
 
 class make_versioneer(Command):
     description = "create standalone versioneer.py"
-    user_options = []
-    boolean_options = []
-    def initialize_options(self):
+    user_options: List[Tuple[str, str, str]] = []
+    boolean_options: List[str] = []
+    def initialize_options(self) -> None:
         pass
-    def finalize_options(self):
+    def finalize_options(self) -> None:
         pass
-    def run(self):
+    def run(self) -> None:
         with open("versioneer.py", "w") as f:
             f.write(generate_versioneer_py().decode("utf8"))
-        return 0
 
 class make_long_version_py_git(Command):
     description = "create standalone _version.py (for git)"
-    user_options = []
-    boolean_options = []
-    def initialize_options(self):
+    user_options: List[Tuple[str, str, str]] = []
+    boolean_options: List[str] = []
+    def initialize_options(self) -> None:
         pass
-    def finalize_options(self):
+    def finalize_options(self) -> None:
         pass
-    def run(self):
+    def run(self) -> None:
         assert os.path.exists("versioneer.py")
         long_version = generate_long_version_py("git")
         with open("git_version.py", "w") as f:
@@ -108,10 +114,9 @@ class make_long_version_py_git(Command):
                      "PARENTDIR_PREFIX": "parentdir_prefix",
                      "VERSIONFILE_SOURCE": "versionfile_source",
                      })
-        return 0
 
 class my_build_py(build_py):
-    def run(self):
+    def run(self) -> None:
         v = generate_versioneer_py()
         v_b64 = base64.b64encode(v).decode("ascii")
         lines = [v_b64[i:i+60] for i in range(0, len(v_b64), 60)]
@@ -124,17 +129,18 @@ class my_build_py(build_py):
             installer.write_text(s)
 
             self.package_dir.update({'': os.path.relpath(installer.parent)})
-            rc = build_py.run(self)
-        return rc
+            build_py.run(self)
 
 # The structure of versioneer, with its components that are compiled into a single file,
 # makes it unsuitable for development mode.
-class develop(develop):
-    def run(self):
+class develop(_develop):
+    def run(self) -> None:  # type: ignore[override]
         raise RuntimeError("Versioneer cannot be installed in developer/editable mode.")
 
 # Bootstrap a versioneer module to guarantee that we get a compatible version
-versioneer = ilu.module_from_spec(ilu.spec_from_loader('versioneer', loader=None))
+versioneer = ilu.module_from_spec(
+    ilu.spec_from_loader('versioneer', loader=None)  # type: ignore[arg-type]
+)
 exec(generate_versioneer_py(), versioneer.__dict__)
 
 VERSION = versioneer.get_version()
